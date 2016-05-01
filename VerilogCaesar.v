939de0b99076828d944eea768c1289cbbd87af5d
Cal Barkman <nahallacman@gmail.com>
2016-04-30 21:46:39 -0700

    Fixed 7 Seg Positions

--- VerilogCaesar.v
+++ VerilogCaesar.v
@@ -6,9 +6,11 @@
 * Using 3 pairs of 7 seg displays.
 *		Left most pair on the board is pair 0, right most is pair 2 (opposite order direction from switches!)
 *			Pair 0 is the key, can never be greater than 25
-				Pair 0 is Hex 0 and Hex 1
+				Pair 0 is Hex 4 and Hex 5
 *			Pair 1 is the plaintext when encrypting, and is the cyhpertext when decrypting
+				Pair 1 is Hex 2 and Hex 3
 *			Pair 2 is cyphertext when encrypting, and is the plaintext when decrypting
+				Pair 2 is Hex 0 and Hex 1
 *		Pair 1 and pair 2 count up by 1 every second (probably only needs to increase one of the two values at a time? or do we increase both values at the same time?)
 */
 
@@ -26,10 +28,10 @@ module VerilogCaesar (CLOCK_50, SW, ENCRYPT, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5)
 	output [0:6] HEX5;
 
 	wire clk_1Hz;
-	wire [3:0] bcd, bcd2, bcd3, bcd4, bcd5, bcd6;
+	wire [3:0] bcd0, bcd1, bcd2, bcd3, bcd4, bcd5;
 	wire [1:0] hundredsFiller;
 	wire [5:0] encryptOutput;
-	wire [5:0] digit_total;
+	//wire [5:0] digit_total;
 	
 	//reg[3:0] digit_flipper;
 	//reg[1:0] digit_flipper2;
@@ -90,57 +92,57 @@ module VerilogCaesar (CLOCK_50, SW, ENCRYPT, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5)
 	//assign bcd = digit_flipper;
 	//assign bcd2 = {2'b0, digit_flipper2};
 	
-	assign digit_total = 6'b000000;
+	//assign digit_total = 6'b000000;
 
 	binary_to_BCD bin_to_bcd0(
-					.A(digit_flipper_ext),
-					.ONES(bcd),
-					.TENS(bcd2),
+					.A(SW),
+					.ONES(bcd4),
+					.TENS(bcd5),
 					.HUNDREDS(hundredsFiller)
 					);
 	
 	binary_to_BCD bin_to_bcd1(
-					.A(SW),
-					.ONES(bcd3),
-					.TENS(bcd4),
+					.A(digit_flipper_ext),
+					.ONES(bcd2),
+					.TENS(bcd3),
 					.HUNDREDS(hundredsFiller)
 					);
 					
 	binary_to_BCD bin_to_bcd2(
 					.A(encryptOutput), // this will have to be attached to a mux later
-					.ONES(bcd5),
-					.TENS(bcd6),
+					.ONES(bcd0),
+					.TENS(bcd1),
 					.HUNDREDS(hundredsFiller)
 					);
 	
 	// drive the display through a 7-seg decoder
 	bcd7seg digit_0(
-				.bcd(bcd), 
+				.bcd(bcd0), 
 				.display(HEX0)
 				);
 				
 	bcd7seg digit_1(
-				.bcd(bcd2), 
+				.bcd(bcd1), 
 				.display(HEX1)
 				);
 				
 	bcd7seg digit_2(
-				.bcd(bcd3), 
+				.bcd(bcd2), 
 				.display(HEX2)
 				);
 				
 	bcd7seg digit_3(
-				.bcd(bcd4), 
+				.bcd(bcd3), 
 				.display(HEX3)
 				);
 				
 	bcd7seg digit_4(
-				.bcd(bcd5), 
+				.bcd(bcd4), 
 				.display(HEX4)
 				);
 				
 	bcd7seg digit_5(
-				.bcd(bcd6), 
+				.bcd(bcd5), 
 				.display(HEX5)
 				);
 				
